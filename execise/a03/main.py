1de8379199d86368b0a641a4aae9c3be6b757c9e
libra189 <libra189@gmail.com>
2023-06-29 06:35:17 +0000

    Add A03 - Two Cards  execise

---
+++ execise/a03/main.py
@@ -0,0 +1,19 @@
+def main():
+    i: list[str] = input().split(" ")
+    n: int = int(i[0])
+    k: int = int(i[1])
+
+    rc: int[str] = [int(j) for j in input().split(" ")]
+    bc: int[str] = [int(j) for j in input().split(" ")]
+
+    for r in rc:
+        for b in bc:
+            # print(f"r: {r}, b: {b}")
+            if k == r + b:
+                print("Yes")
+                return
+
+    print("No")
+
+if __name__ == "__main__":
+    main()
